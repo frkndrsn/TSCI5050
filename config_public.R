1b5015d2490e3a7ab1d8edf8aab76a3f96c34497
frkndrsn <furkandursun@gmail.com>
2021-04-20 12:04:48 -0500

    update

--- config_public.R
+++ config_public.R
@@ -1,4 +1,4 @@
 #this is the simulated version of the data file
 datafile0<-'./Data/NCDB.csv'
-lookup <- 'Data/NCDB_CodeLookup.csv'
+lookup <- './Data/NCDB_CodeLookup.csv'
 if(file.exists('config.R')) source('config.R')
--- data.R
+++ data.R
@@ -5,16 +5,21 @@ library("survival")
 library("ggplot2")
 library('forcats')
 dat0 <- import(datafile0)
+lookup0 <- import(lookup)
+dat1 <- mutate(lookup0, Code=gsub('NAACCR:630:','',Code) %>% as.integer()) %>% left_join(dat0, ., by=c("INSURANCE_STATUS"='Code'))
 #create a new column
-#c_event <- 'PUF_VITAL_STATUS'
-#c_time <- 'DX_LASTCONTACT_DEATH_MONTHS'
-dat0$c_event <- dat0$PUF_VITAL_STATUS == 1
-dat0$c_time <- dat0$DX_LASTCONTACT_DEATH_MONTHS 
-dat0$c_T <- dat0$TNM_CLIN_T %>% gsub('[ABC]','',.) %>% gsub('0','1',.) %>% gsub('^$','cX',.)
+c_event <- 'PUF_VITAL_STATUS'
+c_time <- 'DX_LASTCONTACT_DEATH_MONTHS'
+dat1$c_event <- dat1$PUF_VITAL_STATUS == 1
+dat1$c_time <- dat1$DX_LASTCONTACT_DEATH_MONTHS
+dat1$c_T <- dat1$TNM_CLIN_T %>% gsub('[ABC]','',.) %>% gsub('0','1',.) %>% gsub('^$','cX',.)
 #to see a histogram of the data
-hist(dat0$c_time)
-ggplot(dat0,aes(x=c_time,fill=c_event)) + geom_histogram()
-fit0 <- survfit(Surv(c_time, c_event) ~ c_T, data = dat0) 
+hist(dat1$c_time)
+ggplot(dat1,aes(x=c_time,fill=c_event)) + geom_histogram()
+fit0 <- survfit(Surv(c_time, c_event) ~ c_T, data = dat1)
 plot(fit0,col = c('blue','green','red','grey'))
-legend('topright',legend=names(fit$strata), col = c('blue','green','red','grey'), lty = 1)
+legend('topright',legend=names(fit0$strata), col = c('blue','green','red','grey'), lty = 1)
+fit1 <- survfit(Surv(c_time, c_event) ~ Status, data = dat1)
+plot(fit1,col = c('blue','green','red','grey','orange','purple'))
+legend('bottomleft',legend=names(fit1$strata), col = c('blue','green','red','grey','orange','purple'), lty = 1)
 
